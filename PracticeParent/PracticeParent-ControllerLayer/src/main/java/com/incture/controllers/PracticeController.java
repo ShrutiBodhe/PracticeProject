c7f95950915af5a5aa845b7e73390eb1a2e3ac69
P2001210334 <Shruti.Ghanshyam@incture.com>
2019-12-06 16:50:54 +0530

    deleted changes
    Signed-off-by: P2001210334 <Shruti.Ghanshyam@incture.com>

--- PracticeParent/PracticeParent-ControllerLayer/src/main/java/com/incture/controllers/PracticeController.java
+++ PracticeParent/PracticeParent-ControllerLayer/src/main/java/com/incture/controllers/PracticeController.java
@@ -19,17 +19,6 @@ public class PracticeController {
 		MYLOGGER.info("C6");
 		System.err.println("================================");
 		System.err.println("=================e1===============");
-		System.err.println("=================e2===============");
-		System.err.println("=================e3===============");
-		System.err.println("=================e4===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
-		System.err.println("=================e5===============");
 
 		System.err.println("In Practice Controllers....");
 		System.err.println("Git Chnges testing");
